8ab21f16fc759aa89d21a55f33249681dffbf646
Kyle Prestgard <ktprestgard@gmail.com>
2016-01-18 07:25:35 -0700

    2nd to last problem

--- closures.js
+++ closures.js
@@ -10,11 +10,12 @@ var outer = function(){
 //Invoke outer saving the return value into another variable called 'inner'.
 
   //Code Here
+  var inner = outer();
 
 //Once you do that, invoke inner.
 
   //Code Here
-
+inner();
 
 
 //Next problem
@@ -33,7 +34,8 @@ var callFriend = function(){
 //Do what you need to do in order to call your function and get 'Calling Jake at 435-215-9248' in your console.
 
   //Code Here
-
+  var newF = callFriend();
+newF('435-215-9248');
 
 
 //Next Problem
@@ -45,6 +47,13 @@ var callFriend = function(){
 */
 
   //Code Here
+  var makeCounter = function() {
+    var num = 0;
+    return function() {
+      num++;
+        return num;
+    }
+}
   var count = makeCounter();
   count(); // 1
   count(); // 2
@@ -53,27 +62,63 @@ var callFriend = function(){
 
 
 
+
 //Next Problem
 
 
 
 /*
-  Write a function named codeLove that returns the string 'I love code'. Write a second function named codeFriend that accepts the first function as it's first parameter. The second function should return a new third function. Store the third function in a variable, codeEcho which, when invoked, invokes the first, original function that was passed in, but will only ever do so once (returns null after first invocation).
+  Write a function named codeLove that returns the string 'I love code'. Write a second function named codeFriend that accepts the first function 
+  as it's first parameter. The second function should return a new third function. Store the third function in a variable, codeEcho which, 
+  when invoked, invokes the first, original function that was passed in, but will only ever do so once (returns null after first invocation).
 */
 
   //Code Here
+var codeLove = function() {
+  return('I love code');
+}
+codeLove();
 
+var codeFriend = function(codeLove){
+  var count = 0;
+   return function() {
+    if (count === 0) {
+      count++;
+      return codeLove();
+    }
+    else {
+      return null;
+    }
+  }
+}
 
+var codeEcho = codeFriend(codeLove);
 
+codeEcho();
 //Next Problem
 
 
 
 /*
-  Now, similar to the last problem, write a function called 'fnCounter' that accepts two parameters. The first parameter will be an anonymous function and the second parameter, 'N', will be a number. Now, in 'fnCounter', allow the anonymous funciton to be invoked 'N' number of times. After it's been invoked 'N' number of times, return 'STOP'.
+  Now, similar to the last problem, write a function called 'fnCounter' that accepts two parameters. The first parameter will be an anonymous 
+  function and the second parameter, 'N', will be a number. Now, in 'fnCounter', allow the anonymous funciton to be invoked 'N' number of times. 
+  After it's been invoked 'N' number of times, return 'STOP'.
 */
 
-
+var fnCounter = function(N) {
+  var counter = 0;
+  return function() {
+    if (counter !== N) {
+      counter++;
+      return counter;
+    }
+     else {
+      return 'STOP';
+    }
+  }
+}
+var fin = fnCounter(5);
+fin();
 
 //Next Problem
 
@@ -88,9 +133,11 @@ var callFriend = function(){
     }
   };
 
-  Above you have a function named counter. Examine the function (without running the code) then below write what you expect to happen when the funciton is invoked. *Hint: setTimeout calls a function or evaluates an expression after a specified number of milliseconds.
+  Above you have a function named counter. Examine the function (without running the code) then below write what you expect to happen when the 
+  funciton is invoked. *Hint: setTimeout calls a function or evaluates an expression after a specified number of milliseconds.
 
     //Answer Here
+    print numbers slowly cause of the time out.
 
 
   Now, run the function in your console and note what happpens.
@@ -98,15 +145,27 @@ var callFriend = function(){
   Was your answer right or wrong?
 
     //Answer Here
+    wrong
 
 
-  Fix the counter function so that it works the way you expect it to work. (logging 1 then 2 then 3, etc) (Note: No unit test for this one because of the timeout)
+  Fix the counter function so that it works the way you expect it to work. (logging 1 then 2 then 3, etc) (Note: No unit test for this one 
+  because of the timeout)
 */
 
     //Code Here
 
 
-
+    var counter = function(){
+    for (var i=1; i<=5; i++) {
+      (function() {
+      var num = i;
+      setTimeout( function timer(){
+          console.log( num );
+        }, i*1000 );
+      })();
+    };
+  }
+  counter();
 //Next Problem
 
 
